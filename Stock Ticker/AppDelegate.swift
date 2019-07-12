57f0c94329e5557e7109106b475e6808de1e59e8
ayush4059 <ayush.agrawal@livspace.com>
2019-07-12 12:30:27 +0530

    Initial Commit

---
+++ Stock Ticker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Stock Ticker
+//
+//  Created by Ayush on 12/07/19.
+//  Copyright © 2019 Ayush. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Stock Ticker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stock Ticker
+//
+//  Created by Ayush on 12/07/19.
+//  Copyright © 2019 Ayush. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
